fcbd160001e35ff3b6cb420c0a1d46ca8513ba5c
Dipesh Kafle <dipesh.kaphle111@gmail.com>
2023-06-13 16:55:27 +0530

    remove unnecessary function definition

--- lib/reader_list.ml
+++ lib/reader_list.ml
@@ -15,17 +15,5 @@ let rec add_reader t node =
   let nodes = Atomic.get t in
   if not (Atomic.compare_and_set t nodes (node :: nodes)) then add_reader t node
 
-let[@inline] for_all t f =
-  begin
-    let t' = Atomic.get t in
-    let rec g = function
-      | [] -> ()
-      | x :: xs ->
-        f x;
-        g xs
-    in
-    g t'
-    (* List.iter f t' *)
-  end
-
+let[@inline] for_all t f = List.iter f (Atomic.get t)
 let len t = List.length (Atomic.get t)
