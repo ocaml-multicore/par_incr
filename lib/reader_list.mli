179984e14175ecc14a1dc8949f6272d7b3c7905b
Dipesh Kafle <dipesh.kaphle111@gmail.com>
2023-06-19 16:04:19 +0530

    init gh-pages

--- lib/reader_list.mli
+++
@@ -1,7 +0,0 @@
-type t
-
-val empty : unit -> t
-val add_reader : t -> Rsp.RNode.t -> unit
-val remove_reader : t -> Rsp.RNode.t -> unit
-val for_all : t -> (Rsp.RNode.t -> unit) -> unit
-val len : t -> int
